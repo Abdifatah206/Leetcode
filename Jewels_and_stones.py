3f94fbaa7d54127a8a20562f89aa281efc2f16df
Abdifatah206 <46364693+Abdifatah206@users.noreply.github.com>
2021-05-13 21:28:49 -0700

    Jewels and stones

---
+++ Jewels_and_stones.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        counts = 0
+        for i in jewels:
+            counts += stones.count(i)
+        return counts
+        
+        
